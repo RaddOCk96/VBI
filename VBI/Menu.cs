577786f469573ac11368b7d4705fda78dc65573f
RaddOCk96 <fksamtab@gmail.com>
2025-06-25 11:06:12 -0500

    Cambio de tamaño

---
+++ VBI/Menu.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Emit;
+using System.Windows.Forms;
+
+namespace VBI
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
