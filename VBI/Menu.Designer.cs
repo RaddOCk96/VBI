bf8014c3f2dffe88ef88705e175173bfde4ef34a
RaddOCk96 <fksamtab@gmail.com>
2025-06-25 11:39:09 -0500

    Color/tamaño

--- VBI/Menu.Designer.cs
+++ VBI/Menu.Designer.cs
@@ -28,22 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            label2 = new Label();
             SuspendLayout();
             // 
-            // Form1
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Spectral SemiBold", 49.8000031F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(560, 335);
+            label1.Name = "label1";
+            label1.Size = new Size(274, 128);
+            label1.TabIndex = 0;
+            label1.Text = "ViBio";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Croissant One", 70.2000046F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.DarkGreen;
+            label2.Location = new Point(726, 52);
+            label2.Name = "label2";
+            label2.Size = new Size(412, 167);
+            label2.TabIndex = 1;
+            label2.Text = "ViBio";
+            // 
+            // Menu
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.Highlight;
-            ClientSize = new Size(1253, 760);
+            BackColor = Color.SpringGreen;
+            ClientSize = new Size(1283, 691);
+            Controls.Add(label2);
+            Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             MaximizeBox = false;
-            Name = "Form1";
+            Name = "Menu";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
     }
 }
