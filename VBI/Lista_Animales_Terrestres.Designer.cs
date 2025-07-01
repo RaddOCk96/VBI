ac98259691dbb9adcf7be0db29a46f677aed77fe
981013390 <981013390@LAPTOP-H41KGNPC>
2025-07-01 15:44:38 -0600

    Nueva ventana

--- VBI/Lista_Animales_Terrestres.Designer.cs
+++ VBI/Lista_Animales_Terrestres.Designer.cs
@@ -29,22 +29,86 @@
         private void InitializeComponent()
         {
             iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            panel1 = new Panel();
+            label1 = new Label();
+            iconButton1 = new FontAwesome.Sharp.IconButton();
+            iconButton2 = new FontAwesome.Sharp.IconButton();
+            iconButton3 = new FontAwesome.Sharp.IconButton();
             ((System.ComponentModel.ISupportInitialize)iconPictureBox1).BeginInit();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // iconPictureBox1
             // 
             iconPictureBox1.BackColor = Color.SpringGreen;
             iconPictureBox1.ForeColor = SystemColors.ControlText;
-            iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.NfcDirectional;
+            iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.ArrowLeft;
             iconPictureBox1.IconColor = SystemColors.ControlText;
             iconPictureBox1.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            iconPictureBox1.IconSize = 88;
-            iconPictureBox1.Location = new Point(73, 47);
+            iconPictureBox1.IconSize = 51;
+            iconPictureBox1.Location = new Point(26, 26);
             iconPictureBox1.Name = "iconPictureBox1";
-            iconPictureBox1.Size = new Size(143, 88);
+            iconPictureBox1.Size = new Size(54, 51);
             iconPictureBox1.TabIndex = 0;
             iconPictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(iconButton3);
+            panel1.Controls.Add(iconButton2);
+            panel1.Controls.Add(iconButton1);
+            panel1.Location = new Point(0, 175);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1283, 516);
+            panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Croissant One", 30F);
+            label1.Location = new Point(266, 70);
+            label1.Name = "label1";
+            label1.Size = new Size(787, 71);
+            label1.TabIndex = 2;
+            label1.Text = "Lista de Animales Terrestres";
+            label1.Click += label1_Click;
+            // 
+            // iconButton1
+            // 
+            iconButton1.IconChar = FontAwesome.Sharp.IconChar.None;
+            iconButton1.IconColor = Color.Black;
+            iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton1.Location = new Point(107, 68);
+            iconButton1.Name = "iconButton1";
+            iconButton1.Size = new Size(94, 29);
+            iconButton1.TabIndex = 0;
+            iconButton1.Text = "Agregar";
+            iconButton1.UseVisualStyleBackColor = true;
+            // 
+            // iconButton2
+            // 
+            iconButton2.IconChar = FontAwesome.Sharp.IconChar.None;
+            iconButton2.IconColor = Color.Black;
+            iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton2.Location = new Point(840, 77);
+            iconButton2.Name = "iconButton2";
+            iconButton2.Size = new Size(94, 29);
+            iconButton2.TabIndex = 1;
+            iconButton2.Text = "Editar";
+            iconButton2.UseVisualStyleBackColor = true;
+            // 
+            // iconButton3
+            // 
+            iconButton3.IconChar = FontAwesome.Sharp.IconChar.None;
+            iconButton3.IconColor = Color.Black;
+            iconButton3.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton3.Location = new Point(992, 77);
+            iconButton3.Name = "iconButton3";
+            iconButton3.Size = new Size(94, 29);
+            iconButton3.TabIndex = 2;
+            iconButton3.Text = "Eliminar";
+            iconButton3.UseVisualStyleBackColor = true;
             // 
             // Lista_Animales_Terrestres
             // 
@@ -52,15 +116,24 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.SpringGreen;
             ClientSize = new Size(1283, 691);
+            Controls.Add(label1);
+            Controls.Add(panel1);
             Controls.Add(iconPictureBox1);
             Name = "Lista_Animales_Terrestres";
             Text = "Lista_Animales_Terrestres";
             ((System.ComponentModel.ISupportInitialize)iconPictureBox1).EndInit();
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
+        private Panel panel1;
+        private Label label1;
+        private FontAwesome.Sharp.IconButton iconButton3;
+        private FontAwesome.Sharp.IconButton iconButton2;
+        private FontAwesome.Sharp.IconButton iconButton1;
     }
 }
