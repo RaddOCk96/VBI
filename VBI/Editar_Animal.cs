5f85e1c76c2e805be4c283cc3304633c7ed84b22
981013390 <981013390@LAPTOP-H41KGNPC>
2025-07-04 11:11:59 -0600

    Agregué una nueva ventana llamada Editar_Animal

---
+++ VBI/Editar_Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VBI
+{
+    public partial class Editar_Animal : Form
+    {
+        public Editar_Animal()
+        {
+            InitializeComponent();
+        }
+    }
+}
