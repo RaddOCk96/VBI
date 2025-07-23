9ac981f7339481fc2845f5c4c05085efafc2523c
RaddOCk96 <fksamtab@gmail.com>
2025-07-22 20:05:56 -0500

    no se

--- VBI/obj/Debug/net8.0-windows/VBI.AssemblyInfo.cs
+++ VBI/obj/Debug/net8.0-windows/VBI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VBI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c4c7fce56e1768fdbc7e24785889775bafe72d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bb12860acc572bf92a737e4ff1fa46aa655de67")]
 [assembly: System.Reflection.AssemblyProductAttribute("VBI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VBI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
